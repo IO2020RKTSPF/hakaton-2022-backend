00da46abc44e31ee2336e86685ca8dbb532ac427
rtrydev <r.kulka44@gmail.com>
2022-05-27 19:34:53 +0200

    enterprise model

--- Entities/Enterprise.cs
+++ Entities/Enterprise.cs
@@ -2,5 +2,8 @@ namespace hakaton_2022_backend.Entities;
 
 public class Enterprise
 {
-    
+    public string Name { get; set; }
+    public User Admin { get; set; }
+    public ICollection<User> Users { get; set; }
+    public Config Config { get; set; }
 }
