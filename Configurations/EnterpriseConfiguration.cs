6f6f39358386a77b8e64a85a4300a118a3601779
rtrydev <r.kulka44@gmail.com>
2022-05-27 19:48:23 +0200

    enterprise config

--- Configurations/EnterpriseConfiguration.cs
+++ Configurations/EnterpriseConfiguration.cs
@@ -8,6 +8,14 @@ public class EnterpriseConfiguration : IEntityTypeConfiguration<Enterprise>
 {
     public void Configure(EntityTypeBuilder<Enterprise> builder)
     {
+        builder.ToTable("enterprises").HasKey(x => x.Id);
+
+        builder.HasOne(x => x.Admin);
+
+        builder.HasOne(x => x.Config);
         
+        builder.Property(x => x.Name)
+            .HasColumnName("name")
+            .IsRequired();
     }
 }
