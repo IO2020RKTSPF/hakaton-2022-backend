aa0df921fc3f49528b1facd3619c776d4a34207d
rtrydev <r.kulka44@gmail.com>
2022-05-27 19:54:19 +0200

    config config

--- Configurations/ConfigConfiguration.cs
+++ Configurations/ConfigConfiguration.cs
@@ -8,6 +8,36 @@ public class ConfigConfiguration : IEntityTypeConfiguration<Config>
 {
     public void Configure(EntityTypeBuilder<Config> builder)
     {
+        builder.ToTable("configs").HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .IsRequired()
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.MinutesQuality)
+            .HasColumnName("minutesQuality")
+            .IsRequired();
+
+        builder.Property(x => x.MinutesPerExperience)
+            .HasColumnName("minutesPerExperience")
+            .IsRequired();
+
+        builder.Property(x => x.MinutesPerLines)
+            .HasColumnName("minutesPerLines")
+            .IsRequired();
+
+        builder.Property(x => x.MinutesPerCodeFamiliarity)
+            .HasColumnName("minutesPerCodeFamiliarity")
+            .IsRequired();
+
+        builder.Property(x => x.MinutesPerProjectScale)
+            .HasColumnName("minutesPerProjectScale")
+            .IsRequired();
+
+        builder.Property(x => x.MinutesPerTaskKnowledge)
+            .HasColumnName("minutesPerTaskKnowledge")
+            .IsRequired();
         
     }
 }
