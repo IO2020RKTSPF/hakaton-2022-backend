3038d697839bf4b5a48e7697244e6f11fb602b79
rtrydev <r.kulka44@gmail.com>
2022-05-27 19:31:11 +0200

    user model

--- Entities/User.cs
+++ Entities/User.cs
@@ -2,5 +2,9 @@ namespace hakaton_2022_backend.Entities;
 
 public class User
 {
-    
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public ICollection<Estimation> Estimations { get; set; }
+    public Enterprise Enterprise { get; set; }
 }
