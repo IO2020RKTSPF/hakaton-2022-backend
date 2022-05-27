7ae377470ea1ad2c878538ade72ffb12ea18222c
TomaszSteblik <tsteblik@gmail.com>
2022-05-27 22:24:27 +0200

    added exception middlware

---
+++ Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,20 @@
+using hakaton_2022_backend.Exceptions;
+
+namespace hakaton_2022_backend.Middlewares;
+
+public class ExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            if (e is ApiException apiException)
+                context.Response.StatusCode = apiException.StatusCode;
+        }
+    }
+}
