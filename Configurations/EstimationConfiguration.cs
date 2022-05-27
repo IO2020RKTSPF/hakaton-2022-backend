7971b41f4a308a35a380590bb2f45df4be876e00
rtrydev <r.kulka44@gmail.com>
2022-05-27 19:45:52 +0200

    estimation config

--- Configurations/EstimationConfiguration.cs
+++ Configurations/EstimationConfiguration.cs
@@ -8,6 +8,17 @@ public class EstimationConfiguration : IEntityTypeConfiguration<Estimation>
 {
     public void Configure(EntityTypeBuilder<Estimation> builder)
     {
+        builder.ToTable("estimations").HasKey(x => x.Id);
+
+        builder.HasOne(x => x.Config);
         
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .IsRequired()
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Result)
+            .HasColumnName("result")
+            .IsRequired();
     }
 }
