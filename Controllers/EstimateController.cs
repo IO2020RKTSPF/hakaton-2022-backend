6c65145dbe7a1d1baa4aad8cfedc8de4626d98a2
rtrydev <r.kulka44@gmail.com>
2022-05-27 23:30:31 +0200

    estimation controller

--- Controllers/EstimateController.cs
+++ Controllers/EstimateController.cs
@@ -1,16 +1,44 @@
+using AutoMapper;
 using hakaton_2022_backend.DTOs.Estimate;
+using hakaton_2022_backend.Queries.GetEstimation;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace hakaton_2022_backend.Controllers;
 
 [ApiController]
 [Route("api/estimate")]
-public class EstimateController
+public class EstimateController : ControllerBase
 {
-    [HttpGet]
-    public Task<IActionResult> GetEstimation([FromBody] GetEstimationDto estimationDto)
+    private IMediator _mediator;
+    private IMapper _mapper;
+
+    public EstimateController(IMediator mediator, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [Authorize]
+    [HttpGet]
+    public async Task<IActionResult> GetEstimation([FromBody] GetEstimationDto estimationDto)
+    {
+        var userId = Int32.Parse(User.Claims.First(p => p.Type == "id").Value);
+        var request = new GetEstimationQuery()
+        {
+            UserId = userId,
+            CodeFamiliarity = estimationDto.CodeFamiliarity,
+            DesiredCodeQuality = estimationDto.Quality,
+            ExperienceLevel = estimationDto.Experience,
+            LinesOfCode = estimationDto.Lines,
+            ProjectScale = estimationDto.ProjectScale,
+            TaskKnowledge = estimationDto.TaskKnowledge,
+            UseAi = estimationDto.UseAi
+        };
+
+        var result = await _mediator.Send(request);
+        return Ok(_mapper.Map<EstimationResultDto>(request));
     }
 
     [HttpGet]
