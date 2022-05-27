e6181cfa5d03f2972d2e71dc14e419e91e55c02b
TomaszSteblik <tsteblik@gmail.com>
2022-05-27 19:48:54 +0200

    added auth controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,23 @@
+using hakaton_2022_backend.DTOs.Auth;
+using Microsoft.AspNetCore.Mvc;
+
 namespace hakaton_2022_backend.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class AuthController
 {
-    
+    [HttpPost]
+    [Route(("login"))]
+    public Task<IActionResult> Login([FromBody] LoginUserDto loginUserDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,8 @@
+namespace hakaton_2022_backend.DTOs.Auth;
+
+public class RegisterDto
+{
+    public string OrganizationName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
