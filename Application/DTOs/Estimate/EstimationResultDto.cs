38dfc7c0401d858cbc267154126229ceee5fa173
TomaszSteblik <tsteblik@gmail.com>
2022-05-28 05:19:55 +0200

    added id in estimation

--- Application/DTOs/Estimate/EstimationResultDto.cs
+++ Application/DTOs/Estimate/EstimationResultDto.cs
@@ -2,6 +2,7 @@ namespace hakaton_2022_backend.Application.DTOs.Estimate;
 
 public class EstimationResultDto
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public ParametersDto Parameters { get; set; }
     public int Result { get; set; }
